4e36a60a369f22e64c5bdcf3034f6a0952a87280
biannn999 <216372@daarululuumlido.com>
2026-09-04 17:18:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     // Mengambil semua elemen kotak ekskul
     const ekskulBoxes = document.querySelectorAll(".ekskul-box");
 
